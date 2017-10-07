0bdecff8cf30e7daf8a3ba2e18d4eeeb09d32749
Mustafa Al Kubaisi <mmmshebli@gmail.com>
2017-10-07 07:57:11 -0500

    change from xml config to Java config

---
+++ src/main/java/us/alkubaisi/casemanager/config/AppConfig.java
@@ -0,0 +1,99 @@
+package us.alkubaisi.casemanager.config;
+
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+
+@EnableWebMvc
+@Configuration
+@EnableTransactionManagement
+@PropertySource({ "classpath:persistence-mysql.properties" })
+@ComponentScan("us.alkubaisi.casemanager.*")
+public class AppConfig extends WebMvcConfigurerAdapter{
+	
+	@Autowired
+	private Environment env;
+
+	
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+	
+	@Bean
+	public ComboPooledDataSource myDataSource(){
+		ComboPooledDataSource myDataSource = new ComboPooledDataSource();
+		try {
+			myDataSource.setDriverClass(env.getProperty("driver"));
+		} catch (PropertyVetoException e) {
+			e.printStackTrace();
+		}
+		myDataSource.setJdbcUrl(env.getProperty("jdbcUrl"));
+		myDataSource.setUser(env.getProperty("user"));
+		myDataSource.setPassword(env.getProperty("password"));
+		myDataSource.setMinPoolSize(5);
+		myDataSource.setMaxPoolSize(20);
+		myDataSource.setMaxIdleTime(30000);
+		return myDataSource;
+	}
+	
+	Properties hibernateProperties() {
+		return new Properties() {
+			{
+				setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+				setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+			}
+		};
+	}
+
+	
+	@Bean
+	public LocalSessionFactoryBean sessionFactory(){
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(myDataSource());
+		sessionFactory.setPackagesToScan("us.alkubaisi.casemanager.entity");
+		sessionFactory.setHibernateProperties(hibernateProperties());
+		return sessionFactory;
+	}
+	
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager txManager = new HibernateTransactionManager();
+		txManager.setSessionFactory(sessionFactory);
+
+		return txManager;
+	}
+	
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
+	}
+}
+
+
